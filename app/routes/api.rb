26ce2aaf2e45010316e1068cb0c61314609fde6e
psoldier <soldi.pablo@gmail.com>
2017-08-03 19:34:32 -0300

    Add to api

--- app/routes/api.rb
+++ app/routes/api.rb
@@ -9,6 +9,7 @@ class API < Cuba
         {
           id: index,
           title: category.name,
+          source_name: category.source_name,
           score: 0,
           lifes: 2,
           maxScore: 0,
